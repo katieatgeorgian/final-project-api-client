7a2eb660862224d33219d5bc1377b012bc540920
katieatgeorgian <47111708+katieatgeorgian@users.noreply.github.com>
2020-12-13 14:57:08 -0500

    fixed show/new gallery problem and some styling

--- api/routes/galleries.js
+++ api/routes/galleries.js
@@ -3,7 +3,7 @@ const passport = require('passport');
 
 module.exports = (router) => {
   router.get('/gallery', index);
-  router.get('/gallery/:id', show);
+  router.get('/gallery/view/:id', show);
   router.post('/gallery', passport.authenticate('jwt', { session: false }), create);
   router.post('/gallery/update', passport.authenticate('jwt', { session: false }), update);
   router.post('/gallery/destroy', passport.authenticate('jwt', { session: false }), destroy);
